73de9f0a11ae051721be5bc207fcf520edd07e32
David Steinsland <david@steinsland.email>
2020-03-21 23:18:10 +0100

    oppdaterer rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val githubUser: String by project
 val githubPassword: String by project
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.a468ae5")
+    implementation("com.github.navikt:rapids-and-rivers:1.a75b6ac")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
