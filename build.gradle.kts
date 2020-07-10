616fb4f2d192a33e485bef5c665aff5ccc942016
Martin Solheim <martin.solheim@me.com>
2020-07-10 12:55:04 +0200

    Bumper dependencies og gradle versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val junitJupiterVersion = "5.6.0"
-val mainClass = "no.nav.helse.speider.AppKt"
+val junitJupiterVersion = "5.6.2"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -13,68 +10,55 @@ buildscript {
     }
 }
 
-val githubUser: String by project
-val githubPassword: String by project
+repositories {
+    mavenCentral()
+    maven("https://kotlin.bintray.com/ktor")
+    maven("https://jitpack.io")
+}
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.74ae9cb")
+    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
-repositories {
-    mavenCentral()
-    maven("https://kotlin.bintray.com/ktor")
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/rapids-and-rivers")
-        credentials {
-            username = githubUser
-            password = githubPassword
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+
+    named<Jar>("jar") {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.speider.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
         }
     }
-}
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_12
-    targetCompatibility = JavaVersion.VERSION_12
-}
-
-tasks.named<Jar>("jar") {
-    archiveBaseName.set("app")
-
-    manifest {
-        attributes["Main-Class"] = mainClass
-        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
-            it.name
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
         }
     }
 
-    doLast {
-        configurations.runtimeClasspath.get().forEach {
-            val file = File("$buildDir/libs/${it.name}")
-            if (!file.exists())
-                it.copyTo(file)
-        }
+    withType<Wrapper> {
+        gradleVersion = "6.5.1"
     }
-}
-
-tasks.named<KotlinCompile>("compileKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "6.1"
 }
