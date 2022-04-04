f14c39b9a644dce382f89b8785fc6ec2ab2574b6
David Steinsland <david@steinsland.email>
2022-04-04 20:26:17 +0200

    speider har ikke db

--- src/main/resources/db/migration/V1__initial_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE melding
-(
-    id        BIGSERIAL PRIMARY KEY,
-    data      JSON     NOT NULL,
-    opprettet TIMESTAMP NOT NULL
-);
--- src/main/resources/db/migration/V2__fnr.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE melding ADD COLUMN fnr VARCHAR(32) NOT NULL DEFAULT '';
-CREATE INDEX melding_opprettet_idx ON melding USING BTREE(opprettet);
-DELETE FROM melding WHERE fnr='';
-ALTER TABLE melding ALTER COLUMN fnr DROP DEFAULT;
-
