44aa6f99c3aa98cbda19fb5d0e3d7e8447f11ea2
David Steinsland <david@steinsland.email>
2022-04-05 08:46:20 +0200

    bumper rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.04-22.16.0611abb2a604")
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-08.01.237f724ae572")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
