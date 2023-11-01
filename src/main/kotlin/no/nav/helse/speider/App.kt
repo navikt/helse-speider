fcdd75a6413af6cd5b1f8259fbed397525d28dc7
david steinsland <david@davidsteinsland.net>
2023-11-01 09:40:15 +0100

    ignorerer sparsom enn så lenge

--- src/main/kotlin/no/nav/helse/speider/App.kt
+++ src/main/kotlin/no/nav/helse/speider/App.kt
@@ -25,7 +25,7 @@ private val stateGauge = Gauge.build("app_status", "Gjeldende status på apps")
     .register()
 private val logger = LoggerFactory.getLogger("no.nav.helse.speider.App")
 
-val ignoredApps = setOf("spesialist-migrering")
+val ignoredApps = setOf("sparsom")
 
 fun main() {
     val env = System.getenv()
