300a0d72d449b0a9e61f5437655b24b094ae1255
david steinsland <david@davidsteinsland.net>
2025-03-30 14:22:24 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2025033013081743332933.20de2f3d8983"
+val rapidsAndRiversVersion = "2025033014191743337188.2f9d6b08d096"
 
 plugins {
     kotlin("jvm") version "2.1.20"
