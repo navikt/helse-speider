86ef2178779ad1f021653879033d0244f4dd36c8
david steinsland <david@davidsteinsland.net>
2025-08-05 08:43:04 +0200

    Bump Kotlin JVM -> 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val junitJupiterVersion = "5.12.1"
 val rapidsAndRiversVersion = "2025061811051750237542.df739400e55e"
 
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.2.0"
 }
 
 repositories {
